a44898fd71cbd2754c25d368a190b3a8427b9c39
Webwalker <31214542+wbwlkr@users.noreply.github.com>
2017-11-06 16:24:44 +0100

    Pylint the SLOC to 9,44/10

--- lebonscrap.py
+++ lebonscrap.py
@@ -1,10 +1,19 @@
+#!/usr/bin/env python3
+
+"""Title : LeBonScrap
+Author : wbwlkr (wbwlkr.github.io)
+Description : LeBonScrap is a spider which collect data from www.leboncoin.fr.
+"""
+
 import scrapy
-import json
 
 class LeboncoinSpider(scrapy.Spider):
+    """Crawl all the pagination links to scrap the data of every ads.
+    """
     name = "leboncoin"
     start_urls = [
-        'http://www.leboncoin.fr/li?ca=16_s&c=10&f=p&mre=800&sqs=6&ros=3&ret=1&ret=2&furn=2&location=Toulouse',
+        'http://www.leboncoin.fr/li?'
+        'ca=16_s&c=10&f=p&mre=800&sqs=6&ros=3&ret=1&ret=2&furn=2&location=Toulouse',
     ]
     custom_settings = {
         'COOKIES_ENABLED': False,
@@ -13,11 +22,14 @@ class LeboncoinSpider(scrapy.Spider):
             'Referer': 'https://images.google.com/',
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
             'Accept-Language': 'en',
-            'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36",
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 '
+                          '(KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36',
         }
     }
 
     def parse(self, response_list):
+        """Scrap and parse the page containing the ads and pagination.
+        """
         # follow links to advertissement pages
         for ad_link in response_list.css('a.list_item'):
             description_page = ad_link.css('a::attr("href")').extract_first()
@@ -28,53 +40,31 @@ class LeboncoinSpider(scrapy.Spider):
         if next_list is not None:
             yield response_list.follow(next_list, self.parse)
 
-    def parse_description(self, response_ad):
-        # scrap phone number
-        phone = self.get_phone(response_ad)
+    def parse_description(self, response):
+        """Scrap and parse the content of the ads page.
+        """
+        # check if the phone button exist then return phone number
+        phone_button = response.css('button.button-orange.large.phoneNumber.trackable')\
+                             .extract_first()
+        if phone_button is not None:
+            phone = 'Oui'
+        else:
+            phone = 'Non'
 
         # export data
         return {
-            'Url': response_ad.url,
-            'Titre': response_ad.css("h1::text").extract_first().strip(),
-            'Prix': response_ad.css("h2.item_price.clearfix > span.value::text").extract_first().strip(),
-            'Surface': response_ad.xpath('//div[9]/h2/span[2]/text()').extract_first().strip('m').strip(),
-            'GES': response_ad.xpath('//div[10]/h2/span[2]/a/text()').extract_first(),
-            'Classe énergie': response_ad.xpath('//div[10]/h2/span[2]/a/text()').extract_first(),
-            'Auteur': response_ad.css('a[data-info*="pseudo_annonceur"]::text').extract_first().strip(),
+            'Url': response.url,
+            'Titre': response.css("h1::text").extract_first().strip(),
+            'Prix': response.css("h2.item_price.clearfix > span.value::text")\
+                               .extract_first().strip(),
+            'Surface': response.xpath('//div[9]/h2/span[2]/text()')\
+                                  .extract_first().strip('m').strip(),
+            'GES': response.xpath('//div[10]/h2/span[2]/a/text()')\
+                              .extract_first(),
+            'Classe énergie': response.xpath('//div[10]/h2/span[2]/a/text()')\
+                                         .extract_first(),
+            'Auteur': response.css('a[data-info*="pseudo_annonceur"]::text')\
+                                 .extract_first().strip(),
             'Téléphone': phone,
             'Remarques': ''
         }
-
-    def get_phone(self, res_ad):
-        # check if the phone button exist then return phone number
-        phone_button = res_ad.css('button.button-orange.large.phoneNumber.trackable').extract_first()
-
-        if phone_button is not None:
-            list_id = res_ad.css('input[name="id"]::attr(value)').extract_first().strip()
-            api_key = res_ad.css('section.adview_main > section > script').re('apiKey = "(.+)"')[0]
-
-            request_phone_body = {
-                'list_id': list_id,
-                'app_id': 'leboncoin_web_utils',
-                'key': api_key,
-                'text': '1'
-            }
-
-            request_phone_headers = {
-                'Host': 'api.leboncoin.fr',
-                'Referer': res_ad.url,
-                'Origin': 'https://www.leboncoin.fr',
-            }
-
-            # TODO: Add POST request to phonenumber API
-            # TODO: With url = 'https://api.leboncoin.fr/api/utils/phonenumber.json'
-            # TODO: With body = request_phone_body
-            # TODO: With headers = request_phone_headers
-            # TODO: And parse JSON response with a callback=parse_phone
-            return 'Oui'
-        else:
-            return 'No'
-
-    def parse_phone(self, response_json):
-        jsonresponse = json.loads(response_json)
-        return jsonresponse['utils']['phonenumber']
